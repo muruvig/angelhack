b30a6453624926e02d25f7351d31ef85f9541784
1heart <yixin1996@gmail.com>
2014-07-19 21:05:55 -0700

    adds keyword searching to php

--- scripts/jsonthingy.py
+++ scripts/jsonthingy.py
@@ -2,22 +2,46 @@
 
 from sentscript import *
 import json
+import os
 
-# keyword = json.load(asdflkja;sdkfj; keyword)
-# json.dump(searchFunction(keyword))
+DATA_PATH = os.getcwd()+ '\searching\\'
+keywordFile = ''
 
-bulkData = searchFunction('one')
 
-import httplib,json,urllib
-headers = { "charset":"utf-8", "Accept": "text/plain"}
-conn = httplib.HTTPConnection("localhost")
-#converting list to a json stream
-bulkData = json.dumps(bulkData, ensure_ascii = 'False')
-# ensure_ascii is false as data is in unicode and not ascii encoding , use this if data is in any other encoding
-postData = urllib.urlencode({'results':bulkData})
-conn.request("POST", "/getresult.php", postData,headers)
-# response = conn.getresponse()
-# text = response.read()
-# print (response.status,text)
-# conn.close()
+current = open(DATA_PATH+'keyword.txt', 'r+').read()
+if current != keywordFile:
+	with open(DATA_PATH + "dump.json", "w") as outfile:
+		json.dump(searchFunction(current), outfile)
+	keywordFile = current
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # keyword = json.load(asdflkja;sdkfj; keyword)
+# # json.dump(searchFunction(keyword))
+
+# bulkData = searchFunction('one')
+
+# import httplib,json,urllib
+# headers = { "charset":"utf-8", "Accept": "text/plain"}
+# conn = httplib.HTTPConnection("localhost")
+# #converting list to a json stream
+# bulkData = json.dumps(bulkData, ensure_ascii = 'False')
+# # ensure_ascii is false as data is in unicode and not ascii encoding , use this if data is in any other encoding
+# postData = urllib.urlencode({'results':bulkData})
+# conn.request("POST", "/getresult.php", postData,headers)
+# # response = conn.getresponse()
+# # text = response.read()
+# # print (response.status,text)
+# # conn.close()
 
