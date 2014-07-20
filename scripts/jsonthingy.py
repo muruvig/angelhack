57416ce12d614ae2aa8e080ab627d1e0d5baf29d
Aditya Nekkanti <plainoldindian@gmail.com>
2014-07-19 21:35:25 -0700

    k

--- scripts/jsonthingy.py
+++ scripts/jsonthingy.py
@@ -8,14 +8,14 @@ import json
 
 bulkData = searchFunction('one')
 
-import httplib,json,urllib
+import httplib2, json, urllib
 headers = { "charset":"utf-8", "Accept": "text/plain"}
-conn = httplib.HTTPConnection("localhost")
+conn = httplib2.Http("http://localhost:8888")
 #converting list to a json stream
 bulkData = json.dumps(bulkData, ensure_ascii = 'False')
 # ensure_ascii is false as data is in unicode and not ascii encoding , use this if data is in any other encoding
 postData = urllib.urlencode({'results':bulkData})
-conn.request("POST", "/getresult.php", postData,headers)
+conn.request("POST", "/getresult.php", bulkData,headers)
 # response = conn.getresponse()
 # text = response.read()
 # print (response.status,text)
