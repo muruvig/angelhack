2633aa8cf0b49e79bda80e27ab41d8206ae9dd9e
Vincent <vincent.oddrepublic@gmail.com>
2014-07-19 21:23:34 -0700

    made output dictionary style

--- Scraper/py3Scraper.py
+++ Scraper/py3Scraper.py
@@ -18,37 +18,43 @@ def archive_spider():
         archive_ids += re.compile(r"\"(.*?)\"").findall(str(x))
     archive_list = ["http://www.reuters.com" + str(x) for x in archive_ids]
 
-    return archive_list[::100]
+    return archive_list[:2]
 
-archive_url = archive_spider()
-
-def article_spider():
+def article_spider(archive_url):
     opener = urllib.request.build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36')]
-
-    archive_html = opener.open(archive_url[0]).read()
+    article_links = []
+    archive_html = opener.open(archive_url).read()
     archive_soup = BeautifulSoup(archive_html)
 
     article_list = archive_soup.find("div", class_="primaryContent").find("div", class_="module").find_all("a")
-    article_links = []
     for x in article_list:
         article_links += re.compile(r"<a href=\"(.*?)\">").findall(str(x))
-    return article_links[::100]
+    
+    return article_links[:2]
 
-article_links = article_spider()
-
-def article_parser():
+def article_parser(article_url):
     opener = urllib.request.build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36')]
-
-    article_html = opener.open(article_links[1]).read()
+    
+    article_html = opener.open(article_url).read()
     article_soup = BeautifulSoup(article_html)
-
+    
     if article_soup.find("span", id="articleText").find("pre") == None:
         text = ""
         for string in article_soup.find("span", id="articleText").stripped_strings:
             text += str(string)
     else:
         text = (article_soup.find("span", id="articleText").find("pre").string)
-    
-    return text
+    art_info = [article_soup.find("h1").string, article_url[31:41], text]
+    return art_info
+
+def main():
+    art_dic = {}
+    for archive in archive_spider():
+        for article in article_spider(archive):
+            art_dic[article[31:]] = article_parser(article)
+    return art_dic
+
+if "__name__" = __main__:
+    main()
